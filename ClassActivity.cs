bda36f2c10d47b79c659f3e1d9794496c78bd8d2
Andrea Alheli Gonzalez <11069744@uvu.edu>
2024-05-16 22:31:08 -0600

    Update ClassActivity.cs

--- ClassActivity.cs
+++ ClassActivity.cs
@@ -5,8 +5,20 @@ public class ClassExample
 
 public class ClassWithVariables
 {
-    public int number = 2;
-    public string userName = "Bob";
+    public int houseNumber = 142;
+    Console.WriteLine(houseNumber);
+    
+    public string grandmasName = "Luz Maria & Carolina";
+    Console.WriteLine(grandmasName);
+    
+    public double mySize = 1.59D;
+    Console.WriteLine(mySize);
+    
+    public char lastName = "G";
+    Console.WriteLine(lastName);
+    
+    public bool imHungry = true;
+    Console.WriteLine(imHungry);
 }
 
 public class ClassWithFunctions
