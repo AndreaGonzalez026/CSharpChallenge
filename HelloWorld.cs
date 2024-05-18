e53bf07583bae8406637f4812d08b97eddb09bcf
Andrea Alheli Gonzalez <11069744@uvu.edu>
2024-05-17 19:41:22 -0600

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,5 +1,3 @@
-//Use this file to store your work to demonstrate your understanding of operators
-//Make sure your work is different from the instructors examples or your peers
 using System;
 
 public class HelloWorld
@@ -7,6 +5,6 @@ public class HelloWorld
     public static void Main(string[] args)
     {
         //Change "HW" to "Hello World"
-        Console.WriteLine("HW");
+        Console.WriteLine("HelloClass");
     }    
 }
