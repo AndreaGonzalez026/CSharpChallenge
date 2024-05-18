95fc979cf7c18b29523cfc50502ae6dbdc4784fd
Andrea Alheli Gonzalez <11069744@uvu.edu>
2024-05-17 19:36:22 -0600

    Update CorrectTheErrors.cs

--- CorrectTheErrors.cs
+++ CorrectTheErrors.cs
@@ -1,6 +1,4 @@
-//Use this file to store your work to demonstrate your understanding of operators
-//Make sure your work is different from the instructors examples or your peers
-using Sytem;
+using System;
 
 public class CorrectTheErrors
 {
@@ -8,4 +6,5 @@ public class CorrectTheErrors
     {
         Console.WriteLine("HW);
       
+    }
 }
